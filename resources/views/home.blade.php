3fc50598f588c6c94521cfdaddfe25569cee655c
DarioRossi99 <rssdra99@gmail.com>
2023-01-26 18:15:02 +0100

    fine

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+    <main>
+        <ul>
+            <li><a href="{{route("comics.create")}}">Create</a></li>
+            <li><a href="{{route("comics.index")}}">Index</a></li>
+            {{-- <li><a href="{{route("comics.show"), $comics->$id}}">Show</a></li> --}}
+        </ul>
+    </main>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ComicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", [HomeController::class, "home"])->name("home");
+Route::get("/comics", [ComicController::class, "index"])->name("comics.index");
+Route::get("/comics/create", [ComicController::class, "create"])->name("comics.create");
+Route::post("/comics", [ComicController::class, "store"])->name("comics.store");
+Route::get("/comics/{comics}", [ComicController::class, "show"])->name("comics.show");
